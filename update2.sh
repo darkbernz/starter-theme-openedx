4c649fa1f9d34f681c1e242777b6c8f84b02e0b0
Jose Ibuna <jose.ibuna@corp.nttdataph.com>
2018-02-27 08:59:19 +0800

    Added theme for CMS

---
+++ update2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# update assets as edxapp user
+sudo -H -u edxapp bash << EOF
+source /edx/app/edxapp/edxapp_env
+cd /edx/app/edxapp/edx-platform
+paver update_assets lms --settings=aws --debug
+paver update_assets cms --settings=aws --debug
+EOF
+
+# restart lms instance
+sudo /edx/bin/supervisorctl restart edxapp:lms
+# restart cms instance
+sudo /edx/bin/supervisorctl restart edxapp:cms
